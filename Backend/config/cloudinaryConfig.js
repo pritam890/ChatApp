674a3322adf7eae4dd97c0a97180672a099772a8
Pritam Adhikari <pritamadhikari0001@gmail.com>
2024-10-30 20:13:05 +0530

    fourth commit

--- Backend/config/cloudinaryConfig.js
+++ Backend/config/cloudinaryConfig.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_URL,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET
 });
